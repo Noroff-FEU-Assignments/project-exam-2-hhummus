f4377c8504d9c256571c9a1504fcc70e2a1f974b
Helena <72607635+hhummus@users.noreply.github.com>
2023-04-27 13:25:16 +0200

    added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
 import FeedPage from "./components/pages/FeedPage";
 import LoginRegister from "./components/pages/LoginRegister";
 import ProfilePage from "./components/pages/ProfilePage";
@@ -6,16 +6,16 @@ import UsersPage from "./components/pages/UsersPage";
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="feed" element={<FeedPage />} />
-        <Route path="/index.html" element={<LoginRegister />} />
+        <Route path="/" element={<LoginRegister />} />
         <Route path="/feed/your-profile/:name" element={<ProfilePage />} />
         <Route path="/feed/profiles/:name" element={<UsersPage />} />
 
         <Route path="*" element={<div>feil</div>} /> 
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
